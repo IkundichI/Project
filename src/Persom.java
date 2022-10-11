577b0f1747ecd03d8ea60b4334f7da7e843bba05
Valeev Artem <52968778+IkundichI@users.noreply.github.com>
2022-10-11 19:16:29 +0300

    Secong Commit

---
+++ src/Persom.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Persom {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("I am a woman!");
+    }
+}
